35f9c35ad27f8aa1266e4af8b966ca3bc87dbb0d
angel [itb] navarrete sanchez <angel.navarrete.7e6@itb.cat>
2024-02-25 17:07:41 +0100

    Image Change Functionality + Volume Configuration Added

--- Assets/UIManager.cs
+++ Assets/UIManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
@@ -9,11 +10,19 @@ public class UIManager : MonoBehaviour
     public GameObject startMenu;
     public GameObject dexMenu;
     public GameObject settingsMenu;
+    public GameObject creditsMenu;
+
+    [Header("Start Menu")]
+    public Slider musicSlider;
+    public Slider sfxSlider;
+    public AudioSource musicSource;
+    public AudioSource sfxSource;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        musicSlider.value = musicSource.volume;
+        sfxSlider.value = sfxSource.volume;
     }
 
     public void GoToDex()
@@ -48,5 +57,22 @@ public class UIManager : MonoBehaviour
     {
         SceneManager.LoadScene(0);
         if(dexMenu.activeInHierarchy == false)dexMenu.SetActive(true);
+    }
+
+    public void ShowCredits()
+    {
+        creditsMenu.SetActive(true);
+    }
+    public void HideCreadits()
+    {
+        creditsMenu?.SetActive(false);
+    }
+    public void SetVolume(float volume)
+    {
+        musicSource.volume = volume;
+    }
+    public void SetVolumeSounds(float volume)
+    {
+        sfxSource.volume = volume;
     }
 }
